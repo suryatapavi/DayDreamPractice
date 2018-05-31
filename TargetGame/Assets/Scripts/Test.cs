041fb7cb6c69ca4ae665773ba1d2ad2deeeb1c07
Suryatapa <suryatapa.roy@ucalgary.ca>
2018-05-31 16:28:46 -0600

    Final Commit

---
+++ TargetGame/Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+
+    public float PlayerMoveSpeed = 10;
+    public float PlayerRotateSpeed = 10;
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Getting Started");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GvrControllerInput.ClickButton)
+        {
+            this.transform.Translate(this.transform.forward * PlayerMoveSpeed * Time.deltaTime, Space.World);
+        }
+
+    }
+}
